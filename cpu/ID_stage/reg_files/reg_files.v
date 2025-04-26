f82ce3e7994ff62831f312db9b1543a19067b307
Kanishka Gunawardana <kanishkagunawarthana@gmail.com>
2025-04-26 09:36:25 +0530

    Update the reg files

--- cpu/ID_stage/reg_files/reg_files.v
+++ cpu/ID_stage/reg_files/reg_files.v
@@ -7,19 +7,47 @@ module reg_files(clk, rst, addr1, addr2, data1, data2, we, wd, waddr);
     output [31:0] data1, data2;
 
     reg [31:0] mem[31:0];
-    integer i;
 
     assign #1 data1 = mem[addr1];
     assign #1 data2 = mem[addr2];
 
     always @(negedge clk) begin
-        #1
         if(rst == 1'b1) begin
-            for(i = 0; i < 32; i = i + 1) begin
-                mem[i] <= 32'b0;
-            end
+            #2
+            mem[0] <= 32'b0;
+            mem[1] <= 32'b0;
+            mem[2] <= 32'b0;
+            mem[3] <= 32'b0;
+            mem[4] <= 32'b0;
+            mem[5] <= 32'b0;
+            mem[6] <= 32'b0;
+            mem[7] <= 32'b0;
+            mem[8] <= 32'b0;
+            mem[9] <= 32'b0;
+            mem[10] <= 32'b0;
+            mem[11] <= 32'b0;
+            mem[12] <= 32'b0;
+            mem[13] <= 32'b0;
+            mem[14] <= 32'b0;
+            mem[15] <= 32'b0;
+            mem[16] <= 32'b0;
+            mem[17] <= 32'b0;
+            mem[18] <= 32'b0;
+            mem[19] <= 32'b0;
+            mem[20] <= 32'b0;
+            mem[21] <= 32'b0;
+            mem[22] <= 32'b0;
+            mem[23] <= 32'b0;
+            mem[24] <= 32'b0;
+            mem[25] <= 32'b0;
+            mem[26] <= 32'b0;
+            mem[27] <= 32'b0;
+            mem[28] <= 32'b0;
+            mem[29] <= 32'b0;
+            mem[30] <= 32'b0;
+            mem[31] <= 32'b0;
         end
-        else if(we == 1'b1) begin
+        else if(we == 1'b1 && waddr != 5'd0) begin
             #2 mem[waddr] <= wd;
         end
 

7cd1399d10b9f867d54da378ec8f3ea7eb89a334
KATTA-00 <kanishkagunawarthana@gmail.com>
2025-10-11 13:29:19 +0530

    Update

--- synopsys/cpu_sky130_fd_sc_hd/TZ_OUTDIR/run_func@nominal.pp
+++
@@ -1,7 +0,0 @@
-set_app_var power_enable_rtl_analysis true
-source TZ_OUTDIR/conf_func@nominal.tcl
-::pprtl::read_design_data
-::pprtl::read_name_mapping
-::pprtl::read_activity_files
-::pprtl::compute_metrics
-quit
